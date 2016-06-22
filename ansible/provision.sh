67fb23774d9d7679375aea4e699ce64963dcdfa2
Jason Stillerman <jason.t.stillerman@gmail.com>
2016-06-22 08:25:12 -0400

    update

---
+++ ansible/provision.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -u # Variables must be explicit
+set -e # If any command fails, fail the whole thing
+set -o pipefail
+
+# Make sure SSH knows to use the correct pem
+ssh-add hello-world.pem
+ssh-add -l
+# Load the AWS keys
+. ./inventory/aws_keys
+
+# Ensure the app's cache is up to date
+inventory/ec2.py --refresh-cache
+
+# Tag any existing instances as being old
+ansible-playbook tag-old-nodes.yaml -u ubuntu #--limit tag_Environment_helloworldapp || true
+
+
+echo "DONE!"
+# Start a new instance
+ansible-playbook immutable.yaml -u ubuntu
+
+# Ensure the app's cache is up to date
+inventory/ec2.py --refresh-cache
+
+# Now terminate any instances with tag "old"
+ansible-playbook destroy-old-nodes.yaml -u ubuntu --limit tag_oldhelloworld_True || true
+
+echo "site updated"
